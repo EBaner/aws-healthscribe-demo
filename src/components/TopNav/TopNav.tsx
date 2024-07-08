d64859a0f4c7d93657de1214af511f7f03358840
EBaner <banerjee.ethan@gmail.com>
2024-07-08 14:47:32 -0400

    try again

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -147,7 +147,7 @@ export default function TopNav() {
                     identity={{
                         href: '/',
                         logo: {
-                            src: '/AURIBUSICON.png',
+                            src: '/AURIBUS.png',
                             alt: 'Auribus Technologies',
                         },
                     }}
